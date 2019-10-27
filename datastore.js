f6a71421fe547a945a5044790dd97f8453ae13a6
melissacoy <55949016+melissacoy@users.noreply.github.com>
2019-10-27 08:13:35 -0400

    added question data

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+    {
+        question: "How long was the longest dinosaur ever discovered?",
+        answers: [
+        '120 feet',
+        '70 feet',
+        '150 feet',
+        '100 feet'
+        ],
+        correctAnswer: '120 feet',
+    },
+    {
+        question: "Most dinosaurs were ...?",
+        answers: [
+            'Carnivores (meat-eaters)',
+            'Herbivores (plant-eaters)',
+            'Omnivore (plant and meat eaters)',
+            'Frugivore (fruit-eaters)'
+        ],
+        correctAnswer: 'Herbivores (plant-eaters)',
+    },
+    {
+        question: "The biggest dinosaur skull ever found was the skull of the torosaurus, how long was it? ",
+        answers: [
+            '3 feet',
+            '4 feet',
+            '12 feet',
+            '8 feet'
+        ],
+        correctAnswer: '8 feet',
+    },
+    {
+        question: "The word \"dinosaur\" comes from the Greek words deinos and sauros which translates to:",
+        answers: [
+            'Ancient Warrior',
+            'Terrible Lizard',
+            'Giant Fish',
+            'Secret Dragon'
+        ],
+        correctAnswer: 'Terrible Lizard',
+    },
+    {
+        question: "What year was the first dinosaur fossil discovered and identified?",
+        answers: [
+            '1902',
+            '1921',
+            '1898',
+            '1932'
+          ],
+          correctAnswer: '1902',
+      },
+      {
+        question: "How long ago did dinosaurs become extinct?",
+        answers: [
+            '100 million years ago',
+            '20 million years ago',
+            '65 million years ago',
+            '40 million years ago'
+        ],
+          correctAnswer: '65 million years ago',
+      },
+      {
+        question: "How many dinosaur species have been officially named?",
+        answers: [
+            'Over 10,000',
+            'Over 5,000',
+            'Around 500',
+            'Around 1,000'
+        ],
+          correctAnswer: 'Over 5,000!',
+      },
+      {
+        question: "The smallest discovered dinosaur was the size of a ____?",
+        answers: [
+            'Frog',
+            'Pig',
+            'Chicken',
+            'Rat'
+        ],
+          correctAnswer: 'Frog',
+      },
+      {
+        question: "T-Rex teeth were the size of ____?",
+        answers: [
+            'an elephant Tusk',
+            'a human hand',
+            'a watermelon',
+            'a car'
+        ],
+          correctAnswer: 'a human hand',
+      },
+      {
+        question: "What are scientists who study dinosaurs and fossils called?",
+        answers: [
+            'Jurassicologists',
+            'Fossilologist',
+            'Dinologist',
+            'Paleontologist'
+        ],
+          correctAnswer: 'Paleontologist',
+      },
+    ]
